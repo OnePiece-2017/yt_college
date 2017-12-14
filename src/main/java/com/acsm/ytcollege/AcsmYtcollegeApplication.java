1bb199364480fad8a817c17839ad80b756199b15
李宇 <liyu@acsm.cn>
2017-12-14 19:27:33 +0800

    项目建立

---
+++ src/main/java/com/acsm/ytcollege/AcsmYtcollegeApplication.java
@@ -0,0 +1,12 @@
+package com.acsm.ytcollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcsmYtcollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcsmYtcollegeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/acsm/ytcollege/AcsmYtcollegeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acsm.ytcollege;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcsmYtcollegeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
